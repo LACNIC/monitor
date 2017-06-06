0213607d5d6eb328f88528412be311f45044fc16
Agustín <aguformoso@gmail.com>
2017-06-06 12:10:56 -0300

    Updating admin information

--- monitor/app/admin.py
+++ monitor/app/admin.py
@@ -5,6 +5,24 @@ from app.models import Page, Scan
 from models import Medicion
 
 
-admin.site.register(Medicion)
+class MedicionAdmin(admin.ModelAdmin):
+    pass
+
+
+class MedicionReadOnlyAdmin(MedicionAdmin):
+    """
+        Generic admin covering admin-wide
+    """
+
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in self.model._meta.fields]
+
+class MedicionAdmin(MedicionReadOnlyAdmin):
+    fields = ()
+    list_display = ['date', 'url', 'country_origin']
+    ordering = ['-date']
+    search_fields = ['country_origin', 'url']
+
+admin.site.register(Medicion, MedicionAdmin)
 admin.site.register(Page)
 admin.site.register(Scan)
