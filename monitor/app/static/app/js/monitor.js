5ba9acf8dc5bbde2ee20cb7b11172ecb1835841a
Agustín <aguformoso@gmail.com>
2017-06-05 23:08:35 -0300

    Pushing script

---
+++ monitor/app/static/app/js/monitor.js
@@ -0,0 +1,50 @@
+/**
+ * Created by agustin on 6/5/17.
+ */
+
+(function (f, b) {
+    var c = f.createElement(b),
+        e = f.getElementsByTagName(b)[0];
+    c.src = "https://cdn.dev.lacnic.net/require.js";
+    c.type = "text/javascript";
+    e.parentNode.insertBefore(c, e);
+    c.onload = c.onreadystatechange = function () {
+
+        // Require libraries loaded
+        requirejs.config({
+            "paths": {
+                "boomerang": "https://cdn.rawgit.com/LACNIC/boomerang/master/boomerang",
+                "rt": "https://cdn.rawgit.com/LACNIC/boomerang/master/plugins/rt",
+                "navtiming": "https://cdn.rawgit.com/LACNIC/boomerang/master/plugins/navtiming",
+            }
+        });
+
+        require(["boomerang", "rt", "navtiming"], function () {
+            // Configuration loaded now, safe to do other require calls
+            // that depend on that config.
+
+            //  Launch a blocking fetch before running BOOMR
+            fetch(
+                'https://simon.lacnic.net/getCountry'
+            ).then(function (a) {
+                return a.text();
+            }).then(function (country_origin) {
+
+                //  After knowing the country, run BOOMR
+                BOOMR.subscribe("before_beacon", function (o) {
+
+                    //  Native fetch implementation is not ready for POST yet...
+                    //  Polyfill version neither... :( https://github.com/github/fetch/blob/master/fetch.js
+
+                    o.user_agent = navigator.userAgent;
+                    o.country_origin = country_origin;
+                });
+
+                BOOMR.init({
+                    beacon_url: "http://127.0.0.1:8000/post/",
+                    beacon_type: "POST"
+                });
+            })
+        });
+    }
+}(document, "script"));
