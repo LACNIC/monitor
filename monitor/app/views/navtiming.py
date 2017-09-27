d5c55862ed4543a159d6ab696b29e1a8f06097d1
Agustín <aguformoso@gmail.com>
2017-09-27 15:30:46 -0300

    All 4 charts throught the new JS agent

--- monitor/app/views/navtiming.py
+++ monitor/app/views/navtiming.py
@@ -29,51 +29,24 @@ def navtiming(request):
 
         if rtt <= 0: continue
 
-        rtts[date.replace(minute=0, second=0).strftime("%Y-%m-%dT%H:%M:%S")].append(rtt)
+        rtts[date.replace(minute=0, second=0).strftime("%Y-%m-%d")].append(rtt)
 
-    p90 = list([np.percentile(rtt_list, 90) for rtt_list in rtts.values()])
-    data = dict(
-        x=json.dumps(
-            list(rtts.keys())
-        ),
-        ys=json.dumps([
-            p90,
-            list([np.percentile(rtt_list, 75) for rtt_list in rtts.values()]),
-            list([np.percentile(rtt_list, 50) for rtt_list in rtts.values()]),
-            list([np.percentile(rtt_list, 25) for rtt_list in rtts.values()]),
-            list([np.percentile(rtt_list, 10) for rtt_list in rtts.values()])
-        ]),
-        divId='timeline',
-        labels=json.dumps([
-            '90th perc.',
-            '75th perc.',
-            '50th perc.',
-            '25th perc.',
-            '10th perc.'
-        ]),
-        colors=json.dumps([
-            'red',
-            'orange',
-            'yellow',
-            'green',
-            'blue'
-        ]),
-        kind='LineChart',
-        xType='datetime',
-        my_options=json.dumps({
-            'lineWidth': .25,
-            'chartArea': {
-                'width': "50%"
-            },
-            'vAxis': {
-                'viewWindow': {
-                    'max': np.median(p90)*5.0
-                },
-                'logScale': 'true'
-            }
-        })
-    )
-    timeline = requests.post(settings.CHARTS_URL + '/code/', data=data, headers={'Connection': 'close'}).text
+    p90 = [np.percentile(rtt_list, 90) for rtt_list in rtts.values()]
+    p75 = [np.percentile(rtt_list, 75) for rtt_list in rtts.values()]
+    p50 = [np.percentile(rtt_list, 50) for rtt_list in rtts.values()]
+    p25 = [np.percentile(rtt_list, 25) for rtt_list in rtts.values()]
+    p10 = [np.percentile(rtt_list, 10) for rtt_list in rtts.values()]
+
+    x = list(rtts.keys())
+
+    timeline = {
+        'x': x,
+        'p90': p90,
+        'p75': p75,
+        'p50': p50,
+        'p25': p25,
+        'p10': p10
+    }
 
     rtts = defaultdict(list)
     for m in meds:
@@ -82,51 +55,26 @@ def navtiming(request):
 
         if rtt <= 0: continue
 
-        rtts[date.replace(minute=0, second=0).strftime("%Y-%m-%dT%H:%M:%S")].append(rtt)
+        rtts[date.replace(minute=0, second=0).strftime("%Y-%m-%d")].append(rtt)
 
-    p90 = list([np.percentile(rtt_list, 90) for rtt_list in rtts.values()])
-    data = dict(
-        x=json.dumps(
-            list(rtts.keys())
-        ),
-        ys=json.dumps([
-            p90,
-            list([np.percentile(rtt_list, 75) for rtt_list in rtts.values()]),
-            list([np.percentile(rtt_list, 50) for rtt_list in rtts.values()]),
-            list([np.percentile(rtt_list, 25) for rtt_list in rtts.values()]),
-            list([np.percentile(rtt_list, 10) for rtt_list in rtts.values()])
-        ]),
-        divId='req_timeline',
-        labels=json.dumps([
-            '90 th perc.',
-            '75th perc.',
-            '50th perc.',
-            '25th perc.',
-            '10th perc.'
-        ]),
-        colors=json.dumps([
-            'red',
-            'orange',
-            'yellow',
-            'green',
-            'blue'
-        ]),
-        kind='LineChart',
-        xType='datetime',
-        my_options=json.dumps({
-            'lineWidth': .25,
-            'chartArea': {
-                'width': "50%"
-            },
-            'vAxis': {
-                'viewWindow': {
-                    'max': np.median(p90)*5.0
-                },
-                'logScale': 'true'
-            }
-        })
-    )
-    req_timeline = requests.post(settings.CHARTS_URL + '/code/', data=data, headers={'Connection': 'close'}).text
+    p90 = [np.percentile(rtt_list, 90) for rtt_list in rtts.values()]
+    p75 = [np.percentile(rtt_list, 75) for rtt_list in rtts.values()]
+    p50 = [np.percentile(rtt_list, 50) for rtt_list in rtts.values()]
+    p25 = [np.percentile(rtt_list, 25) for rtt_list in rtts.values()]
+    p10 = [np.percentile(rtt_list, 10) for rtt_list in rtts.values()]
+
+    x = list(rtts.keys())
+
+    req_timeline = {
+        'x': x,
+        'p90': p90,
+        'p75': p75,
+        'p50': p50,
+        'p25': p25,
+        'p10': p10
+    }
+
+
 
     rtts = defaultdict(list)
     for m in meds:
@@ -137,7 +85,7 @@ def navtiming(request):
 
         rtts[date.replace(minute=0, second=0).strftime("%Y-%m-%d")].append(rtt)
 
-    p90 = list([np.percentile(rtt_list, 90) for rtt_list in rtts.values()])
+    p90 = [np.percentile(rtt_list, 90) for rtt_list in rtts.values()]
     p75 = [np.percentile(rtt_list, 75) for rtt_list in rtts.values()]
     p50 = [np.percentile(rtt_list, 50) for rtt_list in rtts.values()]
     p25 = [np.percentile(rtt_list, 25) for rtt_list in rtts.values()]
